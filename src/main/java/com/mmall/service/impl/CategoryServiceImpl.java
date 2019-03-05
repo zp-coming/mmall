8366eb14d6ee05a03b079fbefe5e80bc6238a809
zphh <13979526020@163.com>
2019-03-05 22:01:28 +0800

    分类管理模块

---
+++ src/main/java/com/mmall/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,120 @@
+package com.mmall.service.impl;
+
+import com.google.common.collect.Sets;
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.CategoryMapper;
+import com.mmall.pojo.Category;
+import com.mmall.service.ICategoryService;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author zp
+ * @date 2019/3/3
+ */
+@Service("iCategoryService")
+public class CategoryServiceImpl implements ICategoryService{
+    private Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    /**
+     * 管理员添加品类
+     * @param categoryName
+     * @param parentId
+     * @return
+     */
+    @Override
+    public ServerResponse addCategory(String categoryName, Integer parentId) {
+        if (parentId == null || StringUtils.isBlank(categoryName)) {
+            return ServerResponse.createByErrorMessage("添加品类参数错误");
+        }
+        Category category = new Category();
+        category.setName(categoryName);
+        category.setParentId(parentId);
+        category.setStatus(true); // 这个分类是可用的
+        int rowCount = categoryMapper.insert(category);
+        if (rowCount > 0) {
+            return ServerResponse.createBySuccess("添加品类成功");
+        }
+        return ServerResponse.createByErrorMessage("添加品类失败");
+    }
+
+    /**
+     * 更新品类名字
+     * @param categoryId
+     * @param categoryName
+     * @return
+     */
+    @Override
+    public ServerResponse updateCategoryName(Integer categoryId, String categoryName) {
+        if (categoryId == null || StringUtils.isBlank(categoryName)) {
+            return ServerResponse.createByErrorMessage("添加品类参数错误");
+        }
+        Category category = new Category();
+        category.setId(categoryId);
+        category.setName(categoryName);
+        int rowCount = categoryMapper.updateByPrimaryKeySelective(category);
+        if (rowCount > 0) {
+            return ServerResponse.createBySuccess("更新品类成功");
+        }
+        return ServerResponse.createByErrorMessage("更新品类失败");
+    }
+
+    /**
+     * 查询子节点的category信息,并且不递归,保持平级
+     * @param categoryId
+     * @return
+     */
+    @Override
+    public ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId) {
+        List<Category> categoryList = categoryMapper.selectCategoryChildrenByParentId(categoryId);
+        if (CollectionUtils.isEmpty(categoryList)) {
+            logger.info("未找到当前分类的子分类");
+        }
+        return ServerResponse.createBySuccess(categoryList);
+    }
+
+    /**
+     * 查询当前节点的id和递归子节点的id
+     * @param categoryId
+     * @return
+     */
+    @Override
+    public ServerResponse selectCategoryAndChildrenById(Integer categoryId) {
+        // Set<Category> categorySet = Sets.newHashSet();
+        Set<Category> categorySet = new HashSet<>();
+        findChildCategory(categorySet, categoryId);
+        List<Integer> categoryIdList = new ArrayList<>();
+        if (categoryId != null) {
+            for (Category categoryItem : categorySet) {
+                categoryIdList.add(categoryItem.getId());
+            }
+        }
+        return ServerResponse.createBySuccess(categoryIdList);
+    }
+
+    // 递归算法，算出子节点
+    private void findChildCategory(Set<Category> categorySet, Integer categoryId) {
+        Category category = categoryMapper.selectByPrimaryKey(categoryId);
+        if (category != null) {
+            categorySet.add(category);
+        }
+        // 递归查找子节点，要有退出条件
+        List<Category> categoryList = categoryMapper.selectCategoryChildrenByParentId(categoryId);
+        for (Category categoryItem : categoryList) {
+            findChildCategory(categorySet, categoryItem.getId());
+        }
+    }
+
+}
